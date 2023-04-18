4923e20dac8919e67456fbf659d83aef0862c060
onam <onamkrverma@gmail.com>
2023-04-18 18:40:40 +0530

    :bookmark: add astro image and site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import { astroAWSFunctions } from "@astro-aws/adapter";
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  site: "https://curiosta.com/",
   adapter: astroAWSFunctions(),
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), image()]
 });
