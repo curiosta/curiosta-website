bcbcc59d1ada3b5edf887c07bb18282bb158f8b7
Manoj Sahukar <manojsahukar@users.noreply.github.com>
2023-04-20 13:31:26 +0530

    added cdk pipeline

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,6 +2,7 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { CdkStack } from "../lib/cdk-stack";
+import { CuriostaPipelineStack } from "../lib/pipeline-stack";
 
 const app = new cdk.App();
 new CdkStack(app, "CdkStack", {
@@ -19,3 +20,5 @@ new CdkStack(app, "CdkStack", {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+new CuriostaPipelineStack(app, "PipelineStack", {});
