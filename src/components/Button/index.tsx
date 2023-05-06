64f59c502e3d67666c224313e6fd9d3c38430b77
onam <onamkrverma@gmail.com>
2023-05-06 13:03:56 +0530

    fix: add cx and pass rest in button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,15 @@
 import type { HTMLAttributes } from "preact/compat";
 import type { VariantProps } from "class-variance-authority";
 import button from "@components/Button/button.cva";
+import { cx } from "class-variance-authority";
 
 interface Props
-  extends HTMLAttributes<HTMLButtonElement>,
+  extends Omit<HTMLAttributes<HTMLButtonElement>, "class">,
     VariantProps<typeof button> {}
 
-const Button = ({ type, children, variant, ...rest }: Props) => {
+const Button = ({ type, className, children, ...rest }: Props) => {
   return (
-    <button type={type} class={button({ variant })} {...rest}>
+    <button type={type} class={cx(button(rest), className)} {...rest}>
       {children}
     </button>
   );
