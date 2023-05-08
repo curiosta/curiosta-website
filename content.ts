cc1098d2e9310ae1e8ad0d8670a4c694a609b868
onam <onamkrverma@gmail.com>
2023-05-08 23:45:25 +0530

    feat: create centralize content

---
+++ content.ts
@@ -0,0 +1,63 @@
+export const heroContent = {
+  // Heading of the hero section on the home page
+  heading: `We’re changing the way people connect.`,
+  description: `Cupidatat minim id magna ipsum sint dolor qui. Sunt sit in quis
+  cupidatat mollit aute velit. Et labore commodo nulla aliqua
+  proident mollit ullamco exercitation tempor. Sint aliqua anim
+  nulla sunt mollit id pariatur in voluptate cillum.`,
+};
+
+export const introSection = {
+  // Section below hero
+  heading: `A global platform for local making`,
+  description: `Curiosta is an online marketplace that hosts independently designed
+  furniture and connects its customers to local makers around the world.
+  Rather than mass manufacturing and shipping worldwide, we’re building
+  a distributed and ethical supply chain through a global maker network.`,
+};
+
+// partner companies
+export const partnerCompanies = {
+  heading: "The world’s most innovative companies use our app",
+};
+
+export const socialLinks = {
+  fb: "https://www.facebook.com/",
+  insta: "https://www.instagram.com/",
+  twitter: "https://twitter.com/",
+  github: "https://github.com/",
+  youtube: "https://youtube.com/",
+};
+
+// blog page
+export const blogPage = {
+  heading: "Curiosta blog",
+  description: "Learn how to grow your business with our expert advice.",
+};
+
+// company details
+export const companyInfo = {
+  moto: `Global Innovation, Local Production.`,
+  address: `HD-240, 5th Floor, WeWork Two Horizon Centre, DLF Phase 5, Gurugram, Haryana 122002`,
+  phone: "+91 124 502 5388",
+};
+
+// add page titles
+export const pageTitleDesc = {
+  home: {
+    title: "Curiosta....",
+    description: "where inovation begines",
+  },
+  blog: {
+    title: "blog",
+    description: "Curiosta blog",
+  },
+  signup: {
+    title: "Create account",
+    description: "create your new account",
+  },
+  login: {
+    title: "Login account",
+    description: "login existing account",
+  },
+};
