34297a7e274de551aed6c942f74164b094c53c13
onam <onamkrverma@gmail.com>
2023-05-08 23:29:51 +0530

    add new localstorage key type

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,17 +1,17 @@
 // NOTE: explicitly add keys here so that app does not set or get data from local storage 'accidentally'.
-type LocalStorageKeys = 'cartId';
-
+type LocalStorageKeys = "cartId" | "custId";
 
 /**
-  * A custom hook to simplify the usage of the local storage.
-  *
-  * @function
-  * @returns `get` and `set` method which supports any kind of payload and automatically parses object using JSON.stringify().
-  */
+ * A custom hook to simplify the usage of the local storage.
+ *
+ * @function
+ * @returns `get` and `set` method which supports any kind of payload and automatically parses object using JSON.stringify().
+ */
 const useLocalStorage = () => {
   const setLocalStorage = (key: LocalStorageKeys, payload: any) => {
     try {
-      const value = typeof payload === "object" ? JSON.stringify(payload) : payload;
+      const value =
+        typeof payload === "object" ? JSON.stringify(payload) : payload;
       localStorage.setItem(key, value);
     } catch (error) {
       console.warn(`Error setting local storage key "${key}": `, error);
@@ -21,7 +21,7 @@ const useLocalStorage = () => {
   const getLocalStorage = (key: LocalStorageKeys) => {
     try {
       const value = localStorage.getItem(key);
-      return (value ? JSON.parse(value) : null);
+      return value ? JSON.parse(value) : null;
     } catch (error) {
       console.warn(`Error getting local storage key "${key}": `, error);
       return null;
