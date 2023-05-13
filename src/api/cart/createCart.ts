134353fd18cb441d554680aff6fa3c40b9af2ff9
onam <onamkrverma@gmail.com>
2023-05-13 17:02:15 +0530

    update: add region id in create cart

--- src/api/cart/createCart.ts
+++ src/api/cart/createCart.ts
@@ -1,15 +1,18 @@
 import medusa from "@api/medusa";
 export type CreateCart = {
   country_code?: string;
+  region_id?: string;
   variant_id: string;
   quantity: number;
 };
 export const createCart = async ({
   country_code,
+  region_id,
   variant_id,
   quantity,
 }: CreateCart) => {
   return medusa.carts.create({
+    region_id,
     items: [{ variant_id, quantity }],
   });
 };
