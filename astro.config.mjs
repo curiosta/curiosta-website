b82ecae2297d7063a1ba09e40a743913f5a97b22
onam <onamkrverma@gmail.com>
2023-05-27 13:57:33 +0530

    add astro mdx package

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,17 @@ import preact from "@astrojs/preact";
 import image from "@astrojs/image";
 import aws from "astro-sst/lambda";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://curiosta.com/",
   adapter: aws(),
-  integrations: [tailwind(), preact({ compat: true }), image()],
+  integrations: [tailwind(), preact({
+    compat: true
+  }), image(), mdx()],
   server: {
     // port: 8000
-  },
+  }
 });
