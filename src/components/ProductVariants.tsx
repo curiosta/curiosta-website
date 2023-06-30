9dc094f6d72dc91ed1c5ee0f916194ddabc96a73
onam <onamkrverma@gmail.com>
2023-06-30 20:24:08 +0530

    feat: add limited stock message

--- src/components/ProductVariants.tsx
+++ src/components/ProductVariants.tsx
@@ -1,19 +1,32 @@
-import type { Signal } from "@preact/signals";
+import { useSignal, type Signal } from "@preact/signals";
 import Typography from "@components/Typography";
 import type { PricedVariant } from "@medusajs/medusa/dist/types/pricing";
 import { cx } from "class-variance-authority";
+import { useEffect } from "preact/hooks";
 
 interface Props {
   productVariants?: PricedVariant[];
-  productIndex: Signal<number>
+  productIndex: Signal<number>;
 }
 
 const ProductVariants = ({ productVariants, productIndex }: Props) => {
-  const handleVariant = (
-    index: number
-  ) => {
-    productIndex.value = index
+  const stockMessage = useSignal<string>("");
+
+  const handleVariant = (index: number) => {
+    productIndex.value = index;
   };
+
+  useEffect(() => {
+    stockMessage.value = "";
+    if (!productVariants?.length) return;
+    const selectedVariant = productVariants[productIndex.value];
+    if (
+      selectedVariant.inventory_quantity &&
+      selectedVariant.inventory_quantity <= 10
+    ) {
+      stockMessage.value = "Hurry up! Limited stock available";
+    }
+  }, [productIndex.value]);
 
   return (
     <div>
@@ -27,29 +40,35 @@ const ProductVariants = ({ productVariants, productIndex }: Props) => {
         <div class="grid grid-cols-3 gap-3 sm:grid-cols-6">
           {productVariants?.length
             ? productVariants.map((variant, index) => (
-              <label
-                class={cx(`flex items-center justify-center rounded-md border py-3 px-3 text-sm font-medium uppercase sm:flex-1 cursor-pointer focus:outline-none`, index === productIndex.value
-                  ? "border-transparent bg-primary-600 text-white hover:bg-primary-700"
-                  : "border-gray-200 bg-white text-gray-900 hover:bg-gray-50")}
-              >
-                <input
-                  type="radio"
-                  name="variant-choice"
-                  value={variant.title}
-                  class="sr-only"
-                  onInput={() =>
-                    variant.id && variant.title && variant.prices
-                      ? handleVariant(index)
-                      : undefined
-                  }
-                  aria-labelledby="variant-choice-0-label"
-                />
-                <span id="variant-choice-0-label">{variant.title}</span>
-              </label>
-            ))
+                <label
+                  class={cx(
+                    `flex items-center justify-center rounded-md border py-3 px-3 text-sm font-medium uppercase sm:flex-1 cursor-pointer focus:outline-none`,
+                    index === productIndex.value
+                      ? "border-transparent bg-primary-600 text-white hover:bg-primary-700"
+                      : "border-gray-200 bg-white text-gray-900 hover:bg-gray-50"
+                  )}
+                >
+                  <input
+                    type="radio"
+                    name="variant-choice"
+                    value={variant.title}
+                    class="sr-only"
+                    onInput={() =>
+                      variant.id && variant.title && variant.prices
+                        ? handleVariant(index)
+                        : undefined
+                    }
+                    aria-labelledby="variant-choice-0-label"
+                  />
+                  <span id="variant-choice-0-label">{variant.title}</span>
+                </label>
+              ))
             : "Product variant not available"}
         </div>
       </fieldset>
+      <Typography size="body1/semi-bold" className="mt-4" variant="error">
+        {stockMessage.value}
+      </Typography>
     </div>
   );
 };
