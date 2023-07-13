c8a9e2d504025e4e44b4db2d7c08e3990695af90
onam <onamkrverma@gmail.com>
2023-07-13 16:36:20 +0530

    fix: move cart items in navbar

--- src/components/RightNav.tsx
+++ src/components/RightNav.tsx
@@ -1,57 +1,47 @@
-import cart from "@api/cart";
 import Dropdown from "./Dropdown";
 import Typography from "./Typography";
 import user from "@api/user";
-import Chip from "./Chip";
 
 const RightNav = () => {
-  const totalCartItems = cart.store.value?.items?.reduce(
-    (acc, curVal) => acc + curVal.quantity,
-    0
-  );
-
   return (
     <div>
       <Dropdown title="My Account">
         <Dropdown.Item noHoverEffects tabIndex={-1}>
           <Typography size="body2/normal">
-            {user.state.value === "unauthenticated" ? "Browsing" : "Signed in"} as
+            {user.state.value === "unauthenticated" ? "Browsing" : "Signed in"}{" "}
+            as
           </Typography>
           <Typography size="body2/medium" className="my-0.5" ellipses={1}>
             {user.state.value === "loading" ? "Loading..." : null}
-            {user.state.value === "authenticated" ? user.customer.value?.email : null}
-            {user.state.value === "unauthenticated" ? 'Guest' : null}
+            {user.state.value === "authenticated"
+              ? user.customer.value?.email
+              : null}
+            {user.state.value === "unauthenticated" ? "Guest" : null}
           </Typography>
         </Dropdown.Item>
         <Dropdown.Divider />
-        <Dropdown.Item onClick={() => cart.open.value = true}>
-          <div className="flex justify-between items-center">
-            <Typography>Cart</Typography>
-            <Chip variant='primary2'>
-              {cart.loading.value === 'cart:get' ? 'Loading...' : totalCartItems}
-            </Chip>
-          </div>
-        </Dropdown.Item>
-        <Dropdown.Item onClick={() => location.href = '/orders'}>
+        <Dropdown.Item onClick={() => (location.href = "/orders")}>
           Orders
         </Dropdown.Item>
 
-        {user.state.value !== 'loading' ? <Dropdown.Divider /> : null}
+        {user.state.value !== "loading" ? <Dropdown.Divider /> : null}
         {user.state.value === "authenticated" ? (
-          <Dropdown.Item onClick={async () => {
-            await user.logout()
-            location.href = '/login'
-          }}>
+          <Dropdown.Item
+            onClick={async () => {
+              await user.logout();
+              location.href = "/login";
+            }}
+          >
             Sign out
           </Dropdown.Item>
         ) : null}
         {user.state.value === "unauthenticated" ? (
-          <Dropdown.Item onClick={() => location.href = '/login'}>
+          <Dropdown.Item onClick={() => (location.href = "/login")}>
             Login
           </Dropdown.Item>
         ) : null}
         {user.state.value === "unauthenticated" ? (
-          <Dropdown.Item onClick={() => location.href = '/signup'}>
+          <Dropdown.Item onClick={() => (location.href = "/signup")}>
             Signup
           </Dropdown.Item>
         ) : null}
