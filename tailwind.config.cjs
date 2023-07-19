ee4711f28159672e2bab0499ba5beb9751cb5ed8
onam <onamkrverma@gmail.com>
2023-07-19 13:02:54 +0530

    fix: add custom font weight

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,51 +1,54 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
         primary: colors.indigo,
         danger: colors.red,
         secondray: colors.white,
-        success: colors.green
+        success: colors.green,
       },
       textColor: {
         primary: colors.gray,
-        'app-primary': colors.indigo,
+        "app-primary": colors.indigo,
         disabled: colors.gray[400],
-        success: colors.green
+        success: colors.green,
       },
       screens: {
-        xs: '380px'
+        xs: "380px",
       },
       keyframes: {
-        'mini-expand': {
+        "mini-expand": {
           from: {
             opacity: 0,
-            transform: 'scale(0.95)'
+            transform: "scale(0.95)",
           },
           to: {
             opacity: 1,
-            transform: 'scale(1)'
-          }
+            transform: "scale(1)",
+          },
         },
-        'mini-shrink': {
+        "mini-shrink": {
           from: {
             opacity: 1,
-            transform: 'scale(1)'
+            transform: "scale(1)",
           },
           to: {
             opacity: 0,
-            transform: 'scale(0.95)'
-          }
-        }
-      }
+            transform: "scale(0.95)",
+          },
+        },
+      },
+      fontWeight: {
+        medium: "501",
+      },
     },
     animation: {
-      'mini-expand': 'mini-expand 150ms linear forwards',
-      'mini-shrink': 'mini-shrink 150ms linear forwards'
-    }
+      "mini-expand": "mini-expand 150ms linear forwards",
+      "mini-shrink": "mini-shrink 150ms linear forwards",
+    },
   },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')]
-}
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+};
